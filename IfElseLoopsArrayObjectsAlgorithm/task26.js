31a4d123ae3f2109f0c2db545de9208e28a606e7
Alim <Alim@Fam-MacBook-Air.local>
2024-07-10 23:04:45 +0400

    problem

--- IfElseLoopsArrayObjectsAlgorithm/task26.js
+++ IfElseLoopsArrayObjectsAlgorithm/task26.js
@@ -1,9 +1,8 @@
-let countries = ["Azerbaijan", "Albania", "Germany", "America", "Russia"];
+let countries = ["Azerbaijan", "Albania", "Germany", "America", "Russian"];
 
 function findCountriesStartingAndEndingWithA(arr) {
     let result = arr.filter(country => {
-        return (country.startsWith('A') || country.startsWith('a')) && 
-               (country.endsWith('A') || country.endsWith('a'));
+        return country.startsWith('A') && country.startsWith('a') && country.endsWith('a') && country.endsWith('A');
     });
     return result;
 }
